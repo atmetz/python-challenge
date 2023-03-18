7d0018c08f0d9767defa5507bcbeefad4231ee4b
Andy Metz <atmetz@gmail.com>
2023-03-17 21:00:44 -0500

    Added initial calculations.

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,8 +11,10 @@ budget_csv = os.path.join('Resources', 'budget_data.csv')
 months = 0
 total = 0
 change = 0
+totchange = 0
 greatinc = 0
 greatdec = 0
+prevpl = 0
 greatincmon = ""
 greatdecmon = ""
 
@@ -39,10 +41,41 @@ with open(budget_csv, 'r') as csvfile:
         # Add profit/losses to total
         total += curpl
 
+        # Calculate monthly change
+        change = prevpl - curpl
+
+        totchange += change
+
+        # Check for postive change
+        if change >= 0:
+
+            # Check if current months profit/loss is greater than the current greatest profit/loss
+            if curpl > greatinc:
+
+                # Set the current profit/loss as the greatest and the current month as the greatest month
+                greatinc = curpl
+                greatincmon = curmon
+
+        else:
+
+            # Check if current months profit/loss is greater than the current greatest profit/loss
+            if curpl < greatdec:
+
+                # Set the current profit/loss as the greatest and the current month as the greatest month
+                greatdec = curpl
+                greatdecmon = curmon
+
+        # Set previous months profit/loss for comparison
+        prevpl = curpl
+
+# Calculate average monthly change
+average = round((totchange / months), 2)
+
+# Print Financial Analysis
 print("Financial Analysis")
 print("----------------------------")
 print("Total Months: " + str(months))
 print("Total: $" + str(total))
-print("Average Change: $")
-print("Greatest Increase in Profits:")
-print("Greatest Decrease in Profits:")
+print("Average Change: $" + str(average))
+print("Greatest Increase in Profits: " + greatincmon + " ($" + str(greatinc) + ")")
+print("Greatest Decrease in Profits: " + greatdecmon + " ($" + str(greatdec) + ")")
