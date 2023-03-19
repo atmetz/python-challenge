8b18c1132f86f8036ce2438b8e7b5536620b7057
Andy Metz <atmetz@gmail.com>
2023-03-19 14:47:19 -0500

    Remove hard-coded candidates list. Clean up code and add remaining comments.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,9 +9,9 @@ polls_csv = os.path.join('Resources', 'election_data.csv')
 
 # declare and initialize variables
 total_votes = 0
-candidates = ["Charles Casper Stockham", "Diana DeGette", "Raymon Anthony Doane"]
-candidates_percent = [0, 0, 0]
-candidates_votes = [0, 0, 0]
+candidates = []
+candidates_percent = []
+candidates_votes = []
 election_winner = 0
 
 # Read in csv file
@@ -29,13 +29,27 @@ with open(polls_csv, 'r') as csvfile:
         # Read in the current candidate
         curcand = str(row[2])
 
+        # Set caninlist to false
+        caninlist = False
+
+        # Increment total votes by 1
         total_votes += 1
 
+        # Loop through candidates list
         for i in range(len(candidates)):
 
+            # If the current candidate is in the list, increase their votes by 1
             if candidates[i] == curcand:
                 candidates_votes[i] += 1
+                caninlist = True
 
+        # Check if candidate is in the list. If they are not, add to list
+        if caninlist != True:
+            candidates.append(curcand)
+            candidates_percent.append(0)
+            candidates_votes.append(0)
+
+# Calculate percentage of votes for each candidate, and the winner based on number of votes
 for x in range(len(candidates)):
     candidates_percent[x] = round(((candidates_votes[x] / total_votes) * 100),3)
     if candidates_votes[x] > candidates_votes[election_winner]:
@@ -47,9 +61,25 @@ print("Election Results")
 print("--------------------------")
 print("Total Votes: " + str(total_votes))
 print("--------------------------")
+# Loop through all candidates and print results
 for y in range(len(candidates)):
-
     print(str(candidates[y]) + ": " + str(candidates_percent[y]) + "% (" + str(candidates_votes[y]) + ")")
 print("--------------------------")
 print("Winner: " + candidates[election_winner])
 print("--------------------------")
+
+# Set path for budget_data.csv
+analysis_txt = os.path.join('Analysis', 'analysis.txt')
+
+# Write to text file
+with open(analysis_txt, 'w') as txtfile:
+    # Print Results
+    txtfile.write("Election Results\n")
+    txtfile.write("--------------------------\n")
+    txtfile.write("Total Votes: " + str(total_votes) + "\n")
+    txtfile.write("--------------------------\n")
+    # Loop through all candidates and print results
+    for y in range(len(candidates)):
+        txtfile.write(str(candidates[y]) + ": " + str(candidates_percent[y]) + "% (" + str(candidates_votes[y]) + ")\n")
+    txtfile.write("--------------------------\n")
+    txtfile.write("Winner: " + candidates[election_winner])
